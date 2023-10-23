073a56118eece4b7601531fad53340c5490b09c7
Neal-C <pop6d531@duck.com>
2023-10-23 14:32:05 +0200

    feat(Lexer): added tests cases, 3rd test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -107,3 +107,81 @@ let result = add(five, ten);
 	}
 
 }
+
+//Note that although the input looks like an actual piece of Monkey source code, some lines don’t
+//really make sense, with gibberish like !-/*5. That’s okay. The lexer’s job is not to tell us
+//whether code makes sense, works or contains errors. That comes in a later stage
+//The lexer
+//should only turn this input into tokens
+
+func TestNextToken3(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x,y) {
+x + y ;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	lexer := New(input)
+
+	for i, tt := range tests {
+		tok := lexer.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - Type wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+
+}
