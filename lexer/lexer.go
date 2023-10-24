8767152a3c8bf28330e0ed80988fb0c61b8a8ae6
Neal-C <pop6d531@duck.com>
2023-10-24 16:15:03 +0200

    feat(Lexer): added peekChar Fn

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -130,3 +130,11 @@ func (lexer *Lexer) readNumber() string {
 	}
 	return lexer.input[initialPosition:lexer.position]
 }
+
+func (lexer *Lexer) peekChar() byte {
+	if lexer.readPosition >= len(lexer.input) {
+		return 0
+	} else {
+		return lexer.input[lexer.readPosition]
+	}
+}
