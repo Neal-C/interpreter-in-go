3e6bffa7e2e740ee7d00245e35bd4214cba112d7
Neal-C <pop6d531@duck.com>
2023-10-25 13:48:35 +0200

    feat(REPL): Repl can be launched from main.go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Neal-C/interpreter-in-go/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("goland works")
+	currentUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s ! This is the monkey programming language ! \n", currentUser.Username)
+	fmt.Printf("Start typing commands")
+	repl.Start(os.Stdin, os.Stdout)
 }
