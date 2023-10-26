fcbf02e44cdefcb6c9437a1bed2abe3f386218ea
Neal-C <pop6d531@duck.com>
2023-10-26 10:27:11 +0200

    feat(AST): impl'ed Program struct

--- ast/ast.go
+++ ast/ast.go
@@ -13,3 +13,14 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+type Program struct {
+	Statements []Statement
+}
+
+func (self *Program) TokenLiteral() string {
+	if len(self.Statements) > 0 {
+		return self.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
