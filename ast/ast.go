ac40624d4dc16045af7f96d456b2297382d3b653
Neal-C <pop6d531@duck.com>
2023-10-26 11:07:46 +0200

    feat(AST): page 37 - impl'ed Identifier & LetStatement

--- ast/ast.go
+++ ast/ast.go
@@ -1,18 +1,25 @@
 package ast
 
+import "github.com/Neal-C/interpreter-in-go/token"
+
 type Node interface {
 	TokenLiteral() string
 }
+
+// marker interface$
 
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// marker interface
+
 type Expression interface {
 	Node
 	expressionNode()
 }
+
 type Program struct {
 	Statements []Statement
 }
@@ -23,4 +30,27 @@ func (self *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+type Identifier struct {
+	Token token.Token // The token.IDENT token
+	Value string
+}
+
+func (self *Identifier) expressionNode() {}
+
+func (self *Identifier) TokenLiteral() string {
+	return self.Token.Literal
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (self *LetStatement) statementNode() {}
+
+func (self *LetStatement) TokenLiteral() string {
+	return self.Token.Literal
 }
