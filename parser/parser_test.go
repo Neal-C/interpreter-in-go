06aa46b4e0fc79b4d28084dc9eac29116304d5e2
Neal-C <pop6d531@duck.com>
2023-11-19 10:24:38 +0100

    feat(PARSER):(green): PAGE 79 - added test TestOperatorPrecedence

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -293,3 +293,41 @@ func TestParsingInfixExpressions(t *testing.T) {
 
 	}
 }
+
+func TestOperatorPrecedence(t *testing.T) {
+	// TODO: FIX whitespacing on String Methods !
+	// TODO: the bytes.Buffer thingy
+	tableTest := []struct {
+		input    string
+		expected string
+	}{
+		{"-a * b", "((-a) * b)"},
+		{"!-a", "(!(-a))"},
+		{"a + b + c", "((a + b) + c)"},
+		{"a * b * c", "((a * b) * c)"},
+		{"a * b / c", "((a * b) / c)"},
+		{"a + b / c", "(a + (b / c))"},
+		{
+			"a + b * c + d / e - f",
+			"(((a + (b * c)) + (d / e)) - f)",
+		},
+		{"3 + 4; -5 * 5", "(3 + 4)((-5) * 5)"},
+		{"5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))"},
+		{"5 > 4 != 3 > 4", "((5 > 4) != (3 > 4))"},
+		{"3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))"},
+		{"3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))"},
+	}
+
+	for _, tt := range tableTest {
+		myLexer := lexer.New(tt.input)
+		parser := New(myLexer)
+		program := parser.ParseProgram()
+		checkParserErrors(t, parser)
+
+		actual := program.String()
+
+		if actual != tt.expected {
+			t.Errorf("expected = %q , got = %q", tt.expected, actual)
+		}
+	}
+}
