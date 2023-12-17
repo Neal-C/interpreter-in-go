85addc2baff36daf1e007ceccab81ec70bb5dfaf
Neal-C <pop6d531@duck.com>
2023-12-17 15:32:47 +0100

    feat!(EVALUATOR):(green): PAGE 133 -  impl'd ReturnStatements w/ support for nested IfExpr

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,7 +14,7 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
@@ -29,7 +29,7 @@ func Eval(node ast.Node) object.Object {
 		rightHandSign := Eval(node.Right)
 		return evalInfixExpression(node.Operator, leftHandSign, rightHandSign)
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatements(node)
 	case *ast.IfExpression:
 		return evalIfExpression(node)
 	case *ast.ReturnStatement:
@@ -41,7 +41,7 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
-func evalStatements(stmts []ast.Statement) object.Object {
+func evalProgram(stmts []ast.Statement) object.Object {
 	var result object.Object
 
 	for _, stmt := range stmts {
@@ -159,4 +159,18 @@ func isTruthy(obj object.Object) bool {
 	default:
 		return true
 	}
+}
+
+func evalBlockStatements(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
+	}
+
+	return result
 }
