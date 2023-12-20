e9971a27bf2efa4ded9ff6df08ed562c9d77b148
Neal-C <pop6d531@duck.com>
2023-12-20 15:03:58 +0100

    feat(OBJECT):(red): impl'd NewEnclosedEnvironment

--- object/environment.go
+++ object/environment.go
@@ -2,21 +2,32 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
 	nameToValueMap := make(map[string]Object)
 	return &Environment{
 		store: nameToValueMap,
+		outer: nil,
 	}
 }
 
 func (self *Environment) Get(name string) (Object, bool) {
 	value, ok := self.store[name]
+	if !ok && self.outer != nil {
+		value, ok = self.outer.Get(name)
+	}
 	return value, ok
 }
 
 func (self *Environment) Set(name string, value Object) Object {
 	self.store[name] = value
 	return value
+}
+
+func NewEnclosedEnvironment(outerEnv *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outerEnv
+	return env
 }
