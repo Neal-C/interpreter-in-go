dd4a520d815cf6494644202e6802c4c9683e1f46
Neal-C <pop6d531@duck.com>
2023-12-22 14:58:39 +0100

    feat(PARSER):(red): added TestParsingArrayLiterals

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -702,3 +702,28 @@ func TestStringLiteralExpression(t *testing.T) {
 	}
 
 }
+
+func TestParsingArrayLiterals(t *testing.T) {
+	input := `[1, 2 * 2, 3 + 3]`
+
+	myLexer := lexer.New(input)
+	myParser := New(myLexer)
+	program := myParser.ParseProgram()
+	checkParserErrors(t, myParser)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	array, ok := stmt.Expression.(*ast.ArrayLiteral)
+
+	if !ok {
+		t.Fatalf("expression is not *ast.ArrayLiteral, got = %T", stmt.Expression)
+	}
+
+	if len(array.Elements) != 3 {
+		t.Fatalf("len(array.Elements) not 3, got = %d", len(array.Elements))
+	}
+
+	testIntegerLiteral(t, array.Elements[0], 1)
+	testInfixExpression(t, array.Elements[1], "2", "*", "2")
+	testInfixExpression(t, array.Elements[2], "3", "+", "3")
+
+}
