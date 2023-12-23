331eeb04756d636b9d002bdfe31e37e4ffa4088b
Neal-C <pop6d531@duck.com>
2023-12-23 11:14:36 +0100

    feat(AST): impl'd IndexExpression

--- ast/ast.go
+++ ast/ast.go
@@ -337,3 +337,23 @@ func (self *ArrayLiteral) String() string {
 
 	return out.String()
 }
+
+type IndexExpression struct {
+	Token token.Token // the '[' token
+	Left  Expression
+	Index Expression
+}
+
+func (self *IndexExpression) expressionNode()      {}
+func (self *IndexExpression) TokenLiteral() string { return self.Token.Literal }
+func (self *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(self.Left.String())
+	out.WriteString("[")
+	out.WriteString(self.Index.String())
+	out.WriteString("]")
+
+	return out.String()
+}
